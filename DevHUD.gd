579d4ef9f7fb6ae515fcc779d6ae44758d2eef26
ewewukek <ewewukek@gmail.com>
2018-12-03 02:07:59 +0300

    fps meter

--- DevHUD.gd
+++ DevHUD.gd
@@ -1,12 +1,11 @@
 extends Control
 
-var position_label
 var player
 
 func _ready():
-	position_label = $Label
 	player = get_node("/root/Main/Player")
 
 func _process(delta):
 	var position = player.global_transform.origin
-	position_label.text = "%.2f %.2f %.2f" % [position.x, position.y, position.z]
+	$Position.text = "%.2f %.2f %.2f" % [position.x, position.y, position.z]
+	$FPS.text = str(Engine.get_frames_per_second())
