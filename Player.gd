02dae0c8f292d10b709495cc5fe811fc2d46ac07
ewewukek <ewewukek@gmail.com>
2018-12-03 04:29:09 +0300

    basic walking

--- Player.gd
+++ Player.gd
@@ -1,31 +1,55 @@
 extends KinematicBody
 
+const WALK_SPEED = 1.6
+const WALK_ACCELERATION = WALK_SPEED * 9.0
+const WALK_DECELERATION = WALK_SPEED * 9.0
+const RUN_SPEED = 3.7
+const SPRINT_SPEED = 6.5
+
+var velocity
+
 var MOUSE_SENSITIVITY = deg2rad(0.12)
-var CAMERA_PITCH_LIMIT = deg2rad(90)
+var CAMERA_PITCH_LIMIT = deg2rad(90.0)
 
 var camera
 var neck
 
 func _ready():
+	velocity = Vector3(0.0, 0.0, 0.0)
 	neck = $Neck
 	camera = $Neck/Camera
 
 func _physics_process(delta):
-	var input_move = Vector3()
+	var direction = Vector3()
 
 	if Input.is_action_pressed("move_forward"):
-		input_move.z -= 1.0
+		direction.z -= 1.0
 	if Input.is_action_pressed("move_backward"):
-		input_move.z += 1.0
+		direction.z += 1.0
 	if Input.is_action_pressed("move_left"):
-		input_move.x -= 1.0
+		direction.x -= 1.0
 	if Input.is_action_pressed("move_right"):
-		input_move.x += 1.0
+		direction.x += 1.0
 
-	if input_move.length() > 0:
-		input_move = input_move.normalized()
-		var velocity = global_transform.basis * input_move
-		move_and_slide(velocity, Vector3(0, 1, 0), 0.05, 2, deg2rad(45))
+	var acceleration = delta * WALK_ACCELERATION
+	var deceleration = delta * WALK_DECELERATION
+	var max_velocity = WALK_SPEED
+
+	if direction.x != 0 or direction.z != 0:
+		direction = global_transform.basis * direction.normalized()
+		velocity += acceleration * direction
+		var speed = Vector2(velocity.x, velocity.z).length()
+		if speed > max_velocity:
+			velocity -= (speed - max_velocity) * velocity
+	else:
+		var speed = Vector2(velocity.x, velocity.z).length()
+		if speed > deceleration:
+			velocity -= deceleration * velocity
+		else:
+			velocity.x = 0
+			velocity.z = 0
+
+	move_and_slide(velocity, Vector3(0, 1, 0), 0.05, 4, deg2rad(45))
 
 func _input(event):
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
